be79dd20d5cee16c2649257e071e3124dabc2f2d
Nikita Volobuev <nikitavbv@gmail.com>
2020-11-26 23:56:03 +0200

    feat(report): report structure

---
+++ lighthouserc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ci: {
+    collect: {
+        url: ['https://nikitavbv.com']
+      /* Add configuration here */
+    },
+    upload: {
+      /* Add configuration here */
+    target: 'temporary-public-storage',
+    },
+  },
+};
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::collections::HashMap;
+
+struct LighthouseReport {
+    audits: HashMap<String, LighthouseAudit>
+}
+
+struct LighthouseAudit {
+    id: String,
+    title: String,
+    description: String,
+    score: f64,
+    score_display_mode: String,
+    numeric_value: Option<f64>,
+    numeric_unit: Option<String>,
+    display_value: Option<String>
+}
+
 fn main() {
     println!("Hello, world!");
 }
